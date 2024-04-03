581c853eaa703a85535195bf93e7eb78ebbd1200
yamrajOfCodes <kundanpatil0111@gmail.com>
2024-04-02 21:36:26 -0400

    commit

--- app.js
+++ app.js
@@ -16,6 +16,9 @@ const buildpath=path.join(_dirname,"../frontend/dist")
 app.use(express.static(buildpath));
 app.use(express.json());
 app.use(cors());
+require("dotenv").config({
+    path:'./process.env'
+});
 
 
 
@@ -322,6 +325,6 @@ app.post('/getcart',fetchuser,async(req,res)=>{
 
 dbconnect();
 
-app.listen(PORT,()=>{
+app.listen(process.env.PORT,()=>{
     console.log('server is listening');
 })
